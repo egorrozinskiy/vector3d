47fde205ad012bfbda4cf45bf1e0c35953dd79fc
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-10-20 16:18:26 +0400

    Update vector3d.hpp

--- vector3d.hpp
+++ vector3d.hpp
@@ -9,6 +9,21 @@ public:
     const static vector3d Y;
     const static vector3d Z;
     const static vector3d ZERO;
+    int x;
+    int y;
+    int z;
+    int get_x();
+    int get_y();
+    int get_z();
+
+    vector3d();
+    vector3d( int _x, int _y, int _z) {};
+    vector3d operator+(const vector3d &a){};
+    vector3d operator-(const vector3d &a){};
+    vector3d operator*(const vector3d &a){};
+    vector3d operator^(const vector3d &a){};]
+    vector3d operator
+
 
     // TODO: дописать
 
