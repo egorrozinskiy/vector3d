c3c337324290729366faec6ab8735c93268ce4b3
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-11-02 23:38:11 +0400

    Update vector3d.hpp

--- vector3d.hpp
+++ vector3d.hpp
@@ -9,21 +9,27 @@ public:
     const static vector3d Y;
     const static vector3d Z;
     const static vector3d ZERO;
-    int x;
-    int y;
-    int z;
-    int get_x();
-    int get_y();
-    int get_z();
+    float x;
+    float y;
+    float z;
+    float get_x();
+    float get_y();
+    float get_z();
     
-    vector3d();
-    vector3d( int _x, int _y, int _z) ;
+    vector3d() : x(0), y(0), z(0){};
+//    vector3d( int _x, int _y, int _z) : x(_x), y(_y), z(_z){};
+    vector3d(float _x, float _y, float _z) : x(_x), y(_y), z(_z){};
+    vector3d(const vector3d &o) : x(o.x),y(o.y), z(o.z) {};
     vector3d operator+(const vector3d &a) const;
     vector3d operator-(const vector3d &a) const;
-    vector3d operator*(const vector3d &a) const;
+    vector3d operator-() const;
+    float operator*(const vector3d &a) const;
+    friend vector3d operator*(float f, vector3d &vect);
+    friend vector3d operator*(vector3d &vect, float f);
     vector3d operator^(const vector3d &a) const;
     bool operator==(const vector3d &a) const;
     bool operator!=(const vector3d &a) const;
-    double Length() const;
-    double Normalize() const;
+    double length() const;
+    void normalize();
+};
 #endif
