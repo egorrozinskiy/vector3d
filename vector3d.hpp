7691e293dcdeab4bf368575270f51ca62c1f0cde
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-11-03 15:14:09 +0400

    Update vector3d.hpp

--- vector3d.hpp
+++ vector3d.hpp
@@ -9,23 +9,23 @@ public:
     const static vector3d Y;
     const static vector3d Z;
     const static vector3d ZERO;
-    float x;
-    float y;
-    float z;
-    float get_x();
-    float get_y();
-    float get_z();
-    
+    double x;
+    double y;
+    double z;
+    double get_x();
+    double get_y();
+    double get_z();
+
     vector3d() : x(0), y(0), z(0){};
 //    vector3d( int _x, int _y, int _z) : x(_x), y(_y), z(_z){};
-    vector3d(float _x, float _y, float _z) : x(_x), y(_y), z(_z){};
+    vector3d(double _x, double _y, double _z) : x(_x), y(_y), z(_z){};
     vector3d(const vector3d &o) : x(o.x),y(o.y), z(o.z) {};
     vector3d operator+(const vector3d &a) const;
     vector3d operator-(const vector3d &a) const;
     vector3d operator-() const;
-    float operator*(const vector3d &a) const;
-    friend vector3d operator*(float f, vector3d &vect);
-    friend vector3d operator*(vector3d &vect, float f);
+    double operator*(const vector3d &a) const;
+    friend vector3d operator*(double f, vector3d &vect);
+    friend vector3d operator*(vector3d &vect, double f);
     vector3d operator^(const vector3d &a) const;
     bool operator==(const vector3d &a) const;
     bool operator!=(const vector3d &a) const;
